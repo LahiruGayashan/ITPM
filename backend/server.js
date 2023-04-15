768021ac2ff81e365c00be18881e58460079083f
Lahiru Gayashan <gayashan732@gmail.com>
2023-04-15 09:25:13 +0530

    create vehicles routes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/users");
+const vehicleRoutes =require("./routes/vehicles");
 
 // express app
 const app = express();
@@ -16,14 +17,17 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.use("/api/users", userRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/vehicles', vehicleRoutes);
+
 
 // connect to db
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("connected to database");
-    // listen to port
+
+   // listen to port
     app.listen(process.env.PORT, () => {
       console.log("listening for requests on port", process.env.PORT);
     });
